37a1bcc7b42beafae8a6b487ceb1feb01a48877b
Vsa1968911450 <1968911450@qq.com>
2021-05-06 13:11:16 +0800

    一次不小心的尝试，结果就成功了

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <div id="nav">
+      <p @click="zhongwen">中文</p> |
+      <p  @click="yinwen">英文</p>
+      <div>{{ $t('main.logo') }}</div>
+    </div>
+    <router-view/>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    zhongwen () {
+      localStorage.setItem('lang', 'cn')
+      this.$i18n.locale = 'cn'
+    },
+    yinwen () {
+      localStorage.setItem('lang', 'en')
+      this.$i18n.locale = 'en'
+    }
+  }
+}
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+    <div>{{ $t('main.logo') }}</div>
+    <input type="text"
+           :placeholer="$t('auth.username')" />
+  </div>
+</template>
